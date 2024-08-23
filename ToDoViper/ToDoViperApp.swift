856f26b4565ca2703784f79a35d20a15cb61dbbb
Yury Lebedev <quasaryy@gmail.com>
2024-08-23 19:37:59 +0400

    Initial Commit

---
+++ ToDoViper/ToDoViperApp.swift
@@ -0,0 +1,23 @@
+// ToDoViperApp.swift
+// ToDoViper
+//
+// Created by Yury Lebedev on 23.08.24.
+// Copyright © 2024 www.LebedevApps.com Yury Lebedev. All rights reserved.
+//
+// This code is protected against unauthorized copying and modification.
+// Any use, reproduction, or distribution of this code without prior written
+// permission from the copyright owner is strictly prohibited.
+
+import SwiftUI
+
+@main
+struct ToDoViperApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
